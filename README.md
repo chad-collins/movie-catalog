2d06a25036673e667078fce6636a16f41fabae90
chad-collins <contact@chadcollins.net>
2020-04-04 14:06:11 -0400

    optimized date filter and updated readme

--- README.md
+++ README.md
@@ -20,5 +20,10 @@ npm run build
 npm run lint
 ```
 
+### IMPORTANT!
+```
+This application requires an api key from tmdb.com. After signing up and aquiring a key, create a .env.local file in the root directory and place your api key in the following variable: VUE_APP_TMDB_API_KEY="Your key here with quotations".
+```
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -7,27 +7,27 @@ import { faTh, faList, faLink, faHome, faInfo, faSearch, faWindowClose, faPlay, 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faVuejs } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faTh, faList, faLink, faHome, faInfo, faSearch, faWindowClose, faPlay, faCircle, faVuejs )
+library.add(faTh, faList, faLink, faHome, faInfo, faSearch, faWindowClose, faPlay, faCircle, faVuejs)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
 
 Vue.config.productionTip = false;
 
-Vue.filter("formatImageLink", function(value) {
+Vue.filter("formatImageLink", function (value) {
   return value == undefined
     ? "https://via.placeholder.com/185x278/000000/FFFFFF/?text=No%20Image%20Found"
     : "https://image.tmdb.org/t/p/w300" + value;
 });
 
-Vue.filter("dollars", function(value) {
+Vue.filter("dollars", function (value) {
   return value > 0
     ? "$" + value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
     : "Unknown";
 });
 
-Vue.filter("rating", function(value) {
-  const rating = value /2;
+Vue.filter("rating", function (value) {
+  const rating = value / 2;
   const black = "\u2605";
   const white = "\u2606";
   let stars = [];
@@ -39,47 +39,20 @@ Vue.filter("rating", function(value) {
   return stars.join("");
 });
 
-Vue.filter("date", function(value) {
+Vue.filter("date", function (value) {
   (value == null) ? value = '0000-00-00' : value;
   const year = value.slice(0, 4);
-  const monthNum = value.slice(5, 7);
+  const monthNum = Number(value.slice(5, 7));
   const day = value.slice(8, 10);
-
-  const getMonth = num => {
-    return num == 1
-      ? "January"
-      : num == 2
-      ? "February"
-      : num == 3
-      ? "March"
-      : num == 4
-      ? "April"
-      : num == 5
-      ? "May"
-      : num == 6
-      ? "June"
-      : num == 7
-      ? "July"
-      : num == 8
-      ? "August"
-      : num == 9
-      ? "September"
-      : num == 10
-      ? "October"
-      : num == 11
-      ? "November"
-      : "December";
-  };
-
-  return getMonth(monthNum) + " " + day + ", " + year;
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  return months[ monthNum - 1 ] + " " + day + ", " + year;
 });
 
-Vue.filter("year", function(value) {
-
+Vue.filter("year", function (value) {
   return value ? value.slice(0, 4) : "unknown";
 });
 
-Vue.filter("minutesToHours", function(value) {
+Vue.filter("minutesToHours", function (value) {
   const hours = Math.floor(value / 60);
   const minutes = value % 60;
   return value == null
